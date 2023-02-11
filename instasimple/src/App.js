fc9a37c376cbfdf446c9a5cf36cc04950fe8aae6
Abraham Botica Pantoja <Abrahambp1994@icloud.com>
2023-02-11 20:42:54 +0100

    Componentes y pages ready

--- instasimple/src/App.js
+++ instasimple/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import {Header} from "./components/Header"
+import { LoginUser } from './pages/LoginUser';
+import { RegisterUser } from './pages/RegisterUser';
+import { PublicFeed } from './pages/PublicFeed';
+import { UploadPost } from './pages/UploadPost';
+import { UserProfile } from './pages/UserProfile';
+import { FilterResults } from './pages/FilterResults';
+import {NotFound} from "./pages/NotFound"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<PublicFeed />} />
+        <Route path="/register" element={<RegisterUser />} />
+        <Route path="/login" element={<LoginUser />} />
+        <Route path="/posts" element={<UploadPost />} />
+        <Route path="/users/:id" element={<UserProfile />} />
+        <Route path="/posts?description=a" element={<FilterResults />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ instasimple/src/components/Header.js
@@ -0,0 +1,15 @@
+import {LoginOrRegister} from "./LoginOrRegister"
+import {Link} from "react-router-dom"
+
+export const Header = () => {
+    return (
+        <header>
+      <h1>
+        <Link to={"/"}>Instasimple</Link>
+      </h1>
+      <nav>
+        <LoginOrRegister />
+      </nav>
+    </header>
+    )
+}
